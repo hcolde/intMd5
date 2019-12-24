abc08fec56fac071f2b9b7192e69e13661fba254
hcolde <hcolde@github.com>
2019-12-24 16:18:47 +0800

    add readme & md5.go

---
+++ README.md
@@ -0,0 +1,27 @@
+# intMd5
+
+## How to get
+
+```shell
+go get github.com/hcolde/intMd5
+```
+
+## Demo
+
+```go
+import (
+    "fmt"
+    "github.com/intMd5"
+)
+
+func main() {
+    md5 := "AE439DEDC6A1F7FA7C4C37A01D8A4297"
+    md5Stu := intMd5.getMd5(md5)
+    fmt.Println(md5Stu.md51) //1543945436116761
+    fmt.Println(md5Stu.md52) //7343371014814297
+    fmt.Println(md5Stu.temp) //3349893712
+    
+    md5 = intMd5.reset(md5Stu)
+    fmt.Println(md5) // "AE439DEDC6A1F7FA7C4C37A01D8A4297"
+}
+```
---
+++ md5.go
@@ -0,0 +1,72 @@
+/*
+*Auth:hcolde
+*Date:2019/12/24
+*Getting MD5 as number instead of string.
+*/
+package intmd5
+
+type MD5 struct {
+	md51 string
+	md52 string
+	temp int
+}
+
+func Str2Dec(s string) (num int) {
+	l := len(s)
+	for i := l - 1; i >= 0; i-- {
+		num += (int(s[l-i-1]) & 0xf) << uint8(i)
+	}
+	return
+}
+
+func str2Int(i int32) (string, string) {
+	if i > '9' {
+		return string(i - int32(16)), "1"
+	} else {
+		return string(i), "0"
+	}
+}
+
+func md52int(s string) (b string, t string) {
+	for _, j := range s {
+		m, n := str2Int(j)
+		b += m
+		t += n
+	}
+	return
+}
+
+func getMd5(s string) (md5 MD5) {
+	m, n := md52int(s)
+	md5.md51 = m[:16]
+	md5.md52 = m[16:]
+	md5.temp = Str2Dec(n)
+	return
+}
+
+func reset(md5 MD5) (m string) {
+	temp := md5.temp
+	s := ""
+	for temp > 1 {
+		s = string(temp % 2 + 48) + s
+		temp /= 2
+	}
+	s = "1" + s
+
+	for i, j := range md5.md51 {
+		if s[i] == '1' {
+			m += string(j + 16)
+		} else {
+			m += string(j)
+		}
+	}
+
+	for i, j := range md5.md52 {
+		if s[i+16] == '1' {
+			m += string(j + 16)
+		} else {
+			m += string(j)
+		}
+	}
+	return
+}
